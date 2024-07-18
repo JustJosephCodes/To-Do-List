725413e23adeca92e3b57a02b86e9c3139616371
Joseph Benamaisia <josephbenamaisia@gmail.com>
2024-07-18 15:06:49 +0100

    First commit - commiting all files to the repository

---
+++ to-do-list.js
@@ -0,0 +1,37 @@
+const toDoList = [];
+renderToDoList();
+function renderToDoList() {
+    let toDoListHTML = ``;
+        for (let i = 0; i < toDoList.length; i++) {
+        const toDo = toDoList[i];
+        const html = `
+            <div class="taskname">
+                <div class="task-wrapper">
+                    <p>${toDo}</p>
+                    <div class="delete-wrapper">
+                        <button class="delete" onclick="
+                            toDoList.splice(${i}, 1);
+                            renderToDoList();
+                        ">
+                        <img src="./images/trash-can.svg" alt="">
+                        </button>
+                    </div>
+            </div>        
+        </div>`;
+    toDoListHTML += html;
+    }
+        document.querySelector(`.card-wrapper`).innerHTML = toDoListHTML;
+}
+
+function addToDo() {
+    const inputElement = document.querySelector(`.input-js`);
+    let name = inputElement.value;
+    toDoList.push(name);
+    inputElement.value = "";
+    renderToDoList();
+}
+function keyDown() {
+    if (event.key === `Enter`) {
+    addToDo();
+    }
+}
