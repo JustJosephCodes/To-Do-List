548562eae00e8f17971b6e88e853efe4675c60ca
Joseph Benamaisia <josephbenamaisia@gmail.com>
2024-07-20 23:00:11 +0100

    implemented local storage and optimized code

--- to-do-list.js
+++ to-do-list.js
@@ -1,43 +1,60 @@
-const toDoList = [];
-const addButton = document.querySelector(".add-btn");
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
-renderToDoList();
+document.querySelector('.add-btn').addEventListener("click", () =>{
+    const item = document.querySelector(".input-js");
+    createItem(item);
+})
 
-addButton.addEventListener("click", addToDo)
-
-document.addEventListener("keypress", function (event){
-    if (event.key === `Enter`){
-        addToDo()
+document.querySelector(".input-js").addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        const item = document.querySelector(".input-js");
+        createItem(item)
     }
 })
 
-function renderToDoList() {
-    let toDoListHTML = ``;
-        for (let i = 0; i < toDoList.length; i++) {
-        const toDo = toDoList[i];
-        const html = `
+function displayItems(){
+    let items = ""
+    for(let i = 0; i < itemsArray.length; i++){
+        items += `
             <div class="taskname">
                 <div class="task-wrapper">
-                    <p>${toDo}</p>
-                    <div class="delete-wrapper">
-                        <button class="delete" onclick="
-                            toDoList.splice(${i}, 1);
-                            renderToDoList();
-                        ">
-                        <img src="./images/trash-can.svg" alt="">
-                        </button>
-                    </div>
-            </div>        
-        </div>`;
-    toDoListHTML = html;
+                    <p>${itemsArray[i]}</p>
+                    <button class="delete">
+                    <img src="./images/trash-can.svg" alt="">
+                    </button>
+                </div>        
+            </div>
+        `;
     }
-        document.querySelector(`.card-wrapper`).innerHTML = toDoListHTML;
+    document.querySelector(".card-wrapper").innerHTML = items
+    activateDeleteListeners();
 }
 
-function addToDo() {
-    const inputElement = document.querySelector(`.input-js`);
-    let name = inputElement.value;
-    toDoList.push(name);
-    inputElement.value = "";
-    renderToDoList();
+function activateDeleteListeners(){
+    let deleteBtn = document.querySelectorAll(".delete");
+    deleteBtn.forEach((dB, i) =>{
+        dB.addEventListener("click", () =>{ deleteItem(i)})
+    })
 }
+
+function createItem(item){
+    itemsArray.push(item.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function deleteItem(i){
+    itemsArray.splice(i,1);
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    location.reload()
+}
+
+function updateItem(text, i){
+    itemsArray[i] = text;
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    location.reload()
+}
+
+window.onload = function () {
+    displayItems();
+};
